8a3e2ddedfc51ae4c0e960b0db118c2a2def46af
Omrigan <omrigann@gmail.com>
2016-02-09 21:51:45 +0300

    Model-mk6

--- main.py
+++ main.py
@@ -58,39 +58,9 @@ def stemmer(stemmer, word):
 def textToWordList(txt):
     p_stemmer = EnglishStemmer()
     tokenizer = RegexpTokenizer(r'\w+')
-    badword = [
-        'so',
-        'to',
-        're',
-        've',
-        's',
-        't',
-        'can',
-        'go',
-        'one',
-        'like',
-        'now',
-        'see',
-        'look',
-        'know',
-        'm',
-        'just',
-        'now',
-        'thing',
-        'applaus',
-        'think',
-        'want',
-        'make',
-        'get',
-        'realli',
-        'peopl',
-        'use',
-        'we',
-        'us',
-        'well'
-    ]
+
     stop_w = [stemmer(p_stemmer, i) for i in get_stop_words('en')]
-    badword = [stemmer(p_stemmer, i) for i in badword]
+    badword = [stemmer(p_stemmer, i[:-1]) for i in open('ban.txt', 'r')]
     # r = re.compile('^[а-я]+$')
     txt = txt.lower()
     tokens = tokenizer.tokenize(txt)
@@ -99,6 +69,8 @@ def textToWordList(txt):
     tokens = [i for i in tokens if not i in badword]
     return tokens
 
+
+topics_count = 30
 
 def analysis():
     client = MongoClient()
@@ -109,11 +81,9 @@ def analysis():
     print("WordLists builded")
     dictionary = gensim.corpora.Dictionary(wordLists)
     corpus = [dictionary.doc2bow(text) for text in wordLists]
-    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=15, id2word=dictionary, passes=20)
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=topics_count, id2word=dictionary, passes=20)
     ldamodel.save("my_model")
-    for topic in ldamodel.print_topics(num_words=7):
-        print(topic)
-
+    print('Model builded')
 
 def analysisTalks():
     ldamodel = gensim.models.ldamodel.LdaModel.load("my_model")
@@ -122,7 +92,7 @@ def analysisTalks():
     topics = client.test.topics
     topics.remove()
     topics_dict = {}
-    for topic in ldamodel.show_topics(num_topics=15, num_words=10):
+    for topic in ldamodel.show_topics(num_topics=topics_count, num_words=10):
         topics_dict[topic[0]] = {
             'id': topic[0],
             'keywords': topic[1],
@@ -148,6 +118,7 @@ def analysisTalks():
             rating_sum['speak'] += topic[1]
             rating_sum['view'] += topic[1] * talk['views']
             rating_sum['discuss'] += topic[1] * talk['comments']
+        print("Talk is analysing: ", talk['id'])
     for topic in topics_dict.values():
         topic['ratingPercent']={}
         for rat in topic['rating']:
@@ -167,6 +138,7 @@ def getTopics():
 
 @app.route("/analysis-talks")
 def analysisTalksWeb():
+    analysis()
     analysisTalks()
     return 'ok'
 
@@ -175,5 +147,4 @@ def analysisTalksWeb():
 
 if __name__ == "__main__":
     # parseData()
-    # analysis()
     app.run(host='0.0.0.0', debug=True)
